c71bade3cc7867a7b2f39dff12b648f25dccbe8c
Thiri2 <shuns5199@gmail.com>
2021-09-13 19:58:54 +0630

    day7

--- JS/app.js
+++ JS/app.js
@@ -2,13 +2,9 @@
 function show (message) {
 
     // This is a alert code 
-    
+
     alert(message);
     document.write('<h2>Hello JS</h2>');
     console.log('Hello Js');   
 }
 
-CPU => RAM - memory
-
-
-Programming => 1 input => 2 process => 3 output
